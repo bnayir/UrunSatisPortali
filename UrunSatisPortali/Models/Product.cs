c2bd47f30739bfb28bef72a5e702559ed88eb870
bnayir <burcnayir@gmail.com>
2025-10-23 01:07:38 +0300

    update

---
+++ UrunSatisPortali/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace UrunSatisPortali.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string? ImageUrl { get; set; }
+
+        // Foreign Key ve Navigation Property
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
