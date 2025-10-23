1e5e8865f247d83569fad4a7b24046aaf041d585
bnayir <burcnayir@gmail.com>
2025-10-23 15:44:14 +0300

    update

---
+++ UrunSatisPortali/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrunSatisPortali.Data;
+using UrunSatisPortali.Models;
+
+namespace UrunSatisPortali.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IRepository<Category> _categoryRepo;
+
+        public CategoryController(IRepository<Category> categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+        public ActionResult Index()
+        {
+            var categories = _categoryRepo.GetAll();
+            return View(categories);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _categoryRepo.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+
+    }
+}
---
+++ UrunSatisPortali/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering; 
+using UrunSatisPortali.Data;
+using UrunSatisPortali.Models;
+
+namespace UrunSatisPortali.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IRepository<Product> _productRepo;
+        private readonly IRepository<Category> _categoryRepo;
+
+        public ProductController(IRepository<Product> productRepo, IRepository<Category> categoryRepo)
+        {
+            _productRepo = productRepo;
+            _categoryRepo = categoryRepo;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productRepo.GetAll();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Categories = new SelectList(_categoryRepo.GetAll(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            ModelState.Remove("Category");
+            if (ModelState.IsValid)
+            {
+                _productRepo.Add(product);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Categories = new SelectList(_categoryRepo.GetAll(), "Id", "Name");
+            return View(product);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = _productRepo.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Categories = new SelectList(_categoryRepo.GetAll(), "Id", "Name", product.CategoryId);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            ModelState.Remove("Category");
+
+            if (ModelState.IsValid)
+            {
+                _productRepo.Update(product);
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Categories = new SelectList(_categoryRepo.GetAll(), "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _productRepo.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")] 
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = _productRepo.GetById(id);
+            if (product != null)
+            {
+                _productRepo.Delete(product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+       
+    }
+}
