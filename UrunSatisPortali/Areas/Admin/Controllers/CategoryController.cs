832e2552e277b35d0f8ea14c477a85707edf2d05
bnayir <burcnayir@gmail.com>
2025-10-25 16:41:42 +0300

    update

--- UrunSatisPortali/Areas/Admin/Controllers/CategoryController.cs
+++ UrunSatisPortali/Areas/Admin/Controllers/CategoryController.cs
@@ -10,10 +10,11 @@ namespace UrunSatisPortali.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         private readonly IRepository<Category> _categoryRepo;
-
-        public CategoryController(IRepository<Category> categoryRepo)
+        private readonly IRepository<Product> _productRepo;
+        public CategoryController(IRepository<Category> categoryRepo, IRepository<Product> productRepo)
         {
             _categoryRepo = categoryRepo;
+            _productRepo = productRepo;
         }
         public ActionResult Index()
         {
@@ -36,7 +37,49 @@ namespace UrunSatisPortali.Areas.Admin.Controllers
             return View(category);
         }
 
+        public IActionResult Edit(int id)
+        {
+            var category = _categoryRepo.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _categoryRepo.Update(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            bool hasProducts = _productRepo.GetAll().Any(p => p.CategoryId == id);
+
+            if (hasProducts)
+            {
+                return Json(new { success = false, message = "Bu kategoriye bağlı ürünler varken silemezsiniz!" });
+            }
+
+            var category = _categoryRepo.GetById(id);
+            if (category == null)
+            {
+                return Json(new { success = false, message = "Hata: Kategori bulunamadı." });
+            }
+
+            _categoryRepo.Delete(category);
+
+            return Json(new { success = true, message = "Kategori başarıyla silindi." });
+        }
 
     }
 }
